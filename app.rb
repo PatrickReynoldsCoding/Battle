e479961a44f69101f075086dbccb20e8529d0970
PatrickReynoldsCoding <patrickreynoldscoding@gmail.com>
2022-06-06 17:24:14 +0100

    battle complete!

--- app.rb
+++ app.rb
@@ -11,29 +11,33 @@ class Battle < Sinatra::Base
   end
   
 
-get '/' do
-  erb :index
-end
+  get '/' do
+    erb :index
+  end
 
-post '/names' do
-  player_1 = Player.new(params[:player_1_name])
-  player_2 = Player.new(params[:player_2_name])
-  $game = Game.new(player_1, player_2)
-  redirect '/play'
-end
+  post '/names' do
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
+    redirect '/play'
+  end
 
 
-get '/play' do
-  @game = $game
-  erb :play
-end
+  get '/play' do
+    @game = $game
+    erb :play
+  end
 
-get '/attack' do
-  @game = $game
-  @game.attack(@game.player_2)
-  @game.switch_turns
-  erb :attack
-end
+  get '/attack' do
+    @game = $game
+    @game.attack(@game.opponent_of(@game.current_turn))
+    erb :attack
+  end
+
+  post '/switch-turns' do
+    $game.switch_turns
+    redirect('/play')
+  end
 
 
   # # Start the server if this file is executed directly (do not change the line below)
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -12,13 +12,25 @@ feature 'Attacking' do
   expect(page).to have_content 'Ben attacked Paddy'
   end
 
-  scenario 'reduce hp by 10' do
+  scenario 'reduce player 2 hp by 10' do
   sign_in_and_play
   click_button 'Attack'
   click_button 'Continue'
   save_and_open_page
   expect(page).not_to have_content 'Ben: 60HP'
   expect(page).to have_content 'Ben: 50HP'
-  
+  end
+
+  # As Player 1,
+  # So I can start to lose a game of Battle,
+  # I want Player 2's attack to reduce my HP by 10
+  scenario 'reduce Player 1 HP by 10' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_button 'Continue'
+    click_button 'Attack'
+    click_button 'Continue'
+    expect(page).not_to have_content 'Paddy: 60HP'
+    expect(page).to have_content 'Paddy: 50HP'
   end
 end
